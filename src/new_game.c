816eea3d478f3048fc3b7010c0f26f9f98f75593
robertarin <robbie.arin@gmail.com>
2023-07-27 13:02:57 -0700

    Add SuncloakDepths B1F for Coal-driven tutorial

--- src/new_game.c
+++ src/new_game.c
@@ -128,7 +128,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToStart(void)
 {
-    SetWarpDestination(MAP_GROUP(SUNCLOAK_TOWN), MAP_NUM(SUNCLOAK_TOWN), WARP_ID_NONE, 26, 9);
+    SetWarpDestination(MAP_GROUP(SUNCLOAK_DEPTHS_B1F), MAP_NUM(SUNCLOAK_DEPTHS_B1F), WARP_ID_NONE, 22, 7);
     WarpIntoMap();
 }
 
